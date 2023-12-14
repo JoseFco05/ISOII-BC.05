79c05d1ede898df488be8a3c4b6ebcd6dd93462e
mgomez20code <91663380+mgomez20code@users.noreply.github.com>
2023-12-14 19:20:27 +0100

    Añadido testing it4

---
+++ LAB/it4/src/test/java/AlumnoTest.java
@@ -0,0 +1,27 @@
+package ISOII_BC.LAB;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class AlumnoTest {
+
+	@Test
+	public void testAlumno() {
+		assertTrue(true);
+		//throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testAsistir_Evento() {
+		assertTrue(true);
+		//throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testConsultar_Asistencia() {
+		assertTrue(true);
+		//throw new RuntimeException("not yet implemented");
+	}
+
+}
---
+++ LAB/it4/src/test/java/ProfesorTest.java
@@ -0,0 +1,27 @@
+package ISOII_BC.LAB;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class ProfesorTest {
+
+	@Test
+	public void testProfesor() {
+		assertTrue(true);
+		//throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testOrganizar_Evento() {
+		assertTrue(true);
+		//throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testConsultar_Eventos() {
+		assertTrue(true);
+		//throw new RuntimeException("not yet implemented");
+	}
+
+}
