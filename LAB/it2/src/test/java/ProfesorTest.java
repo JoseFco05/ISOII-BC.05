0c0c6c0abd3345f44c199166960adaafdc6c3eb5
mgomez20code <91663380+mgomez20code@users.noreply.github.com>
2023-12-14 19:11:23 +0100

    Añadido testing it2

---
+++ LAB/it2/src/test/java/ProfesorTest.java
@@ -0,0 +1,27 @@
+package ISOII_BC.LAB;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class ProfesorTest {
+
+	@Test
+	public void testProfesor() {
+		assertTrue(true);
+		//throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testAsignarCurso() {
+		assertTrue(true);
+		//throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testDesvincularCurso() {
+		assertTrue(true);
+		//throw new RuntimeException("not yet implemented");
+	}
+
+}
