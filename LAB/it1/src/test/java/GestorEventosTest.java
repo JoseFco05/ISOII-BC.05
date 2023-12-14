763bcf681c2ffee0b31b15c6f180553a93757e90
mgomez20code <91663380+mgomez20code@users.noreply.github.com>
2023-12-14 12:40:53 +0100

    Añadido testing it1

---
+++ LAB/it1/src/test/java/GestorEventosTest.java
@@ -0,0 +1,32 @@
+package ISOII_BC.LAB;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
+
+class GestorEventosTest {
+
+	@Test
+	public void testAnadirEventos() {
+		assertTrue(true);
+		//throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testEditarEvento() {
+		assertTrue(true);
+		//throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testBorrarEventos() {
+		assertTrue(true);
+		//throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testMostrarInformacion() {
+		assertTrue(true);
+		//throw new RuntimeException("not yet implemented");
+	}
+
+}
