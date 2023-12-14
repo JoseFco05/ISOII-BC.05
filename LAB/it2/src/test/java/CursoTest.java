3433ff485ec771a0c41fffc3f6d36900a5b46bcc
mgomez20code <91663380+mgomez20code@users.noreply.github.com>
2023-12-14 12:45:41 +0100

    Añadido testing it2

---
+++ LAB/it2/src/test/java/CursoTest.java
@@ -0,0 +1,15 @@
+package ISOII_BC.LAB;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class CursoTest {
+
+	@Test
+	public void testMostrarAsignaciones() {
+		assertTrue(true);
+		//throw new RuntimeException("not yet implemented");
+	}
+
+}
