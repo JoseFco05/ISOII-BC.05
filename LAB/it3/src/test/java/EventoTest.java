31839e6f99de78c3a7316b3a55cc145340da92cb
mgomez20code <91663380+mgomez20code@users.noreply.github.com>
2023-12-14 12:45:20 +0100

    Añadido testing it3

---
+++ LAB/it3/src/test/java/EventoTest.java
@@ -0,0 +1,4 @@
+package ISOII_BC.LAB;
+
+class EventoTest {
+}
---
+++ LAB/it3/src/test/java/Firma_DigitalTest.java
@@ -0,0 +1,21 @@
+package ISOII_BC.LAB;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class Firma_DigitalTest {
+
+	@Test
+	public void testCrear_Firma() {
+		assertTrue(true);
+		//throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testVerificar_Firma() {
+		assertTrue(true);
+		//throw new RuntimeException("not yet implemented");
+	}
+
+}
