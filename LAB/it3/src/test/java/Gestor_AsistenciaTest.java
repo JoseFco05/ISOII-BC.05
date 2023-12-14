3099c3501034050bd28623318ae1e1cc474fe9cc
mgomez20code <91663380+mgomez20code@users.noreply.github.com>
2023-12-14 19:11:44 +0100

    Añadido testing it3

---
+++ LAB/it3/src/test/java/Gestor_AsistenciaTest.java
@@ -0,0 +1,21 @@
+package ISOII_BC.LAB;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class Gestor_AsistenciaTest {
+
+	@Test
+	public void testGenerar_Reporte() {
+		assertTrue(true);
+		//throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testCrear_Documento() {
+		assertTrue(true);
+		//throw new RuntimeException("not yet implemented");
+	}
+
+}
