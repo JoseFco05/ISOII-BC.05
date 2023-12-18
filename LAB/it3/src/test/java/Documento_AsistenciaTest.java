76d1bdf67f96014dceab4aef5654110ac34677ea
mgomez20code <91663380+mgomez20code@users.noreply.github.com>
2023-12-18 18:18:05 +0100

    Añadido testing it3

---
+++ LAB/it3/src/test/java/Documento_AsistenciaTest.java
@@ -0,0 +1,27 @@
+package ISOII_BC.LAB;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class Documento_AsistenciaTest {
+
+	@Test
+	public void testGenerarPDF() {
+		assertTrue(true);
+		//throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testFirmar_Documento() {
+		assertTrue(true);
+		//throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testDescargar_Documento() {
+		assertTrue(true);
+		//throw new RuntimeException("not yet implemented");
+	}
+
+}
