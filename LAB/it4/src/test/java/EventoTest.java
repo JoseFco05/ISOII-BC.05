f475e2d797ca3e7cb15c466d1b42da5248e5ff62
mgomez20code <91663380+mgomez20code@users.noreply.github.com>
2023-12-18 18:18:42 +0100

    Añadido testing it4

---
+++ LAB/it4/src/test/java/EventoTest.java
@@ -0,0 +1,27 @@
+package ISOII_BC.LAB;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class EventoTest {
+
+	@Test
+	public void testConsultar_Media() {
+		assertTrue(true);
+		//throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testConsultar_Asistencia() {
+		assertTrue(true);
+		//throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testConsultar_Satisfaccion() {
+		assertTrue(true);
+		//throw new RuntimeException("not yet implemented");
+	}
+
+}
---
+++ LAB/it4/src/test/java/InformeTest.java
@@ -0,0 +1,27 @@
+package ISOII_BC.LAB;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class InformeTest {
+
+	@Test
+	public void testInforme_Actividad() {
+		assertTrue(true);
+		//throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testInforme_Asistencia() {
+		assertTrue(true);
+		//throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testInforme_Eventos() {
+		assertTrue(true);
+		//throw new RuntimeException("not yet implemented");
+	}
+
+}
